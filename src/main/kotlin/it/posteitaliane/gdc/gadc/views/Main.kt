f5fba3241d053dec941eafc3689384d5b2cc56af
beppune <manzo.giuseppe@gmail.com>
2024-03-01 21:22:58 +0100

    basic fields for orderform

--- src/main/kotlin/it/posteitaliane/gdc/gadc/views/Main.kt
+++ src/main/kotlin/it/posteitaliane/gdc/gadc/views/Main.kt
@@ -2,16 +2,22 @@ package it.posteitaliane.gdc.gadc.views
 
 import com.vaadin.flow.component.orderedlayout.VerticalLayout
 import com.vaadin.flow.router.Route
+import it.posteitaliane.gdc.gadc.config.GMDConfig
 import it.posteitaliane.gdc.gadc.services.BackOffice
 import it.posteitaliane.gdc.gadc.views.forms.OrderForm
 import it.posteitaliane.gdc.gadc.views.forms.OrderLineForm
+import org.springframework.beans.factory.annotation.Autowired
 
 @Route
-class Main(bo:BackOffice) : VerticalLayout() {
+class Main(bo:BackOffice, config:GMDConfig) : VerticalLayout() {
 
     init {
 
-        val order = OrderForm()
+        val order = OrderForm(
+                bo.dcs.findAll(true),
+                bo.sups.findAll(),
+                config
+            )
         add(order)
     }
 }
--- src/main/kotlin/it/posteitaliane/gdc/gadc/views/forms/OrderForm.kt
+++ src/main/kotlin/it/posteitaliane/gdc/gadc/views/forms/OrderForm.kt
@@ -1,18 +1,32 @@
 package it.posteitaliane.gdc.gadc.views.forms
 
+import com.vaadin.flow.component.combobox.ComboBox
 import com.vaadin.flow.component.formlayout.FormLayout
 import com.vaadin.flow.component.select.Select
+import com.vaadin.flow.component.textfield.TextField
 import com.vaadin.flow.data.binder.Binder
+import com.vaadin.flow.data.binder.ValidationException
+import com.vaadin.flow.data.binder.ValidationResult
+import it.posteitaliane.gdc.gadc.config.GMDConfig
+import it.posteitaliane.gdc.gadc.model.Datacenter
 import it.posteitaliane.gdc.gadc.model.Order
+import it.posteitaliane.gdc.gadc.model.Supplier
 
-class OrderForm : FormLayout() {
+class OrderForm(private val dcs:List<Datacenter>, private val sups:List<Supplier>, private val config:GMDConfig) : FormLayout() {
 
     private val binder:Binder<OrderPresentation>
 
     private var bean:OrderPresentation
 
     private var typeField:Select<Order.Type>
+
     private var subjectField:Select<Order.Subject>
+
+    private var dcSelect:Select<Datacenter>
+
+    private var refField:TextField
+
+    private var supplierField:ComboBox<Supplier>
 
     init {
         bean = OrderPresentation()
@@ -21,7 +35,8 @@ class OrderForm : FormLayout() {
 
         typeField = Select<Order.Type>()
             .apply {
-                setItems(Order.Type.values().asList())
+                placeholder = "CARICO / SCARICO"
+                setItems(Order.Type.entries)
                 setItemLabelGenerator {
                     when(it) {
                         Order.Type.INBOUND -> "CARICO"
@@ -37,6 +52,7 @@ class OrderForm : FormLayout() {
 
         subjectField = Select<Order.Subject>()
             .apply {
+                placeholder = "TIPO DI ORDINE"
                 setItems(Order.Subject.values().asList())
                 setItemLabelGenerator {
                     when(it) {
@@ -51,10 +67,59 @@ class OrderForm : FormLayout() {
             .asRequired("Obbligatorio")
             .bind({it.subject}, { order, subject -> order.subject = subject})
 
+        dcSelect = Select<Datacenter>()
+            .apply {
+                placeholder = "DATACENTER"
+                setItems(dcs)
+                setItemLabelGenerator {
+                    "${it.short} - ${it.fullName}"
+                }
+            }
+
+        binder.forField(dcSelect)
+            .asRequired("Obbligatorio")
+            .bind({it.datacenter},{order, dc -> order.datacenter = dc})
+
+        refField = TextField()
+            .apply {
+                placeholder = "REFERENTE"
+            }
+
+        binder.forField(refField)
+            .bind({it.ref}, { order, ref -> order.ref = ref })
+
+        supplierField = ComboBox<Supplier>()
+            .apply {
+                setItems(sups)
+                setItemLabelGenerator {it.name}
+            }
+
+        binder.forField(supplierField)
+            .bind({it.supplier}, { order, sup -> order.supplier = sup})
+
         binder.readBean(bean)
 
-        add(typeField, subjectField)
+        subjectField.addValueChangeListener {
+            if( it.source.value == Order.Subject.INTERNAL || it.source.value == Order.Subject.SUPPLIER_DC ) {
+                supplierField.value = sups.find { it.name == config.firmName }
+                supplierField.isEnabled = false
+            } else {
+                supplierField.value = null
+                supplierField.isEnabled = true
 
+            }
+        }
+
+        add(typeField, subjectField, dcSelect, refField, supplierField)
+
+    }
+
+    fun writeBean() {
+        try {
+            binder.writeBean(bean)
+        } catch (ex:ValidationException) {
+            println(ex)
+        }
     }
 
 }
