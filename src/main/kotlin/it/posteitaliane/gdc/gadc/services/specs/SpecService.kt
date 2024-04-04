3923608066614340f712a32e2c4697f661eaa037
Giuseppe Manzo <manzo.giuseppe@gmail.com>
2024-04-04 13:05:56 +0200

    fix inbound unique bug

--- src/main/kotlin/it/posteitaliane/gdc/gadc/services/specs/SpecService.kt
+++ src/main/kotlin/it/posteitaliane/gdc/gadc/services/specs/SpecService.kt
@@ -19,33 +19,59 @@ class SpecService(
             ( ss.findBySn(it.sn) ?: ss.findByPt(it.pt) ) == null
         }
     }
-    private val UNIQUE_MUST_BE_IN_STORAGE:OrderPredicate = {UNIQUE_MUST_NOT_BE_IN_STORAGE().not()}
+    private val UNIQUE_MUST_BE_IN_STORAGE:OrderPredicate = {
+        lines.all {
+            if( it.isUnique ) {
+                (ss.findBySn(it.sn) ?: ss.findByPt(it.pt)) != null
+            }
+            true
+        }
+    }
 
-    private val INTERNAL_AMEND:Order.()->Unit = {
-        supplier = sups.findByName(config.firmName)
+    private val AMEND_UNIQUE_AMOUNT:Order.()->Unit = {
         lines.forEach { line ->
             if(line.isUnique) line.amount = 1
         }
     }
 
-    val INBOUND_INTERNAL = OrderSpec()
-        .amend(INTERNAL_AMEND)
+    private val AMEND_INTERNAL_SUPPLIER:Order.()->Unit = {
+        supplier = sups.findByName(config.firmName)
+    }
+
+    val INBOUND_INTERNAL_SPEC = OrderSpec()
+        .amend(AMEND_INTERNAL_SUPPLIER)
+        .amend(AMEND_UNIQUE_AMOUNT)
         .define("UNIQUE_MUST_NOT_BE_IN_STORAGE", UNIQUE_MUST_NOT_BE_IN_STORAGE)
 
-    val OUTBOUND_INTERNAL = OrderSpec()
-        .amend(INTERNAL_AMEND)
+    val OUTBOUND_INTERNAL_SPEC = OrderSpec()
+        .amend(AMEND_INTERNAL_SUPPLIER)
+        .amend(AMEND_UNIQUE_AMOUNT)
         .define("UNIQUE_MUST_BE_IN_STORAGE", UNIQUE_MUST_BE_IN_STORAGE)
 
+    val INBOUND_SUPPLIER_SPEC = OrderSpec()
+        .amend(AMEND_UNIQUE_AMOUNT)
+        .define("UNIQUE_MUST_NOT_BE_IN_STORAGE", UNIQUE_MUST_NOT_BE_IN_STORAGE)
 
+    val OUTBOUND_SUPPLIER_SPEC = OrderSpec()
+        .amend(AMEND_UNIQUE_AMOUNT)
+        .define("UNIQUE_MUST_BE_IN_STORAGE", UNIQUE_MUST_BE_IN_STORAGE)
 
     fun run(order: Order): Pair<List<SpecBit>,List<SpecBit>> {
 
         if(order.type == Order.Type.INBOUND && order.subject == Order.Subject.INTERNAL){
-            return INBOUND_INTERNAL.runSpec(order)
+            return INBOUND_INTERNAL_SPEC.runSpec(order)
         }
 
         if(order.type == Order.Type.OUTBOUND && order.subject == Order.Subject.INTERNAL){
-            return OUTBOUND_INTERNAL.runSpec(order)
+            return OUTBOUND_INTERNAL_SPEC.runSpec(order)
+        }
+
+        if(order.type == Order.Type.INBOUND && order.subject == Order.Subject.SUPPLIER){
+            return INBOUND_SUPPLIER_SPEC.runSpec(order)
+        }
+
+        if(order.type == Order.Type.OUTBOUND && order.subject == Order.Subject.SUPPLIER){
+            return OUTBOUND_SUPPLIER_SPEC.runSpec(order)
         }
 
         throw RuntimeException("Unknown type/subject: $order")
