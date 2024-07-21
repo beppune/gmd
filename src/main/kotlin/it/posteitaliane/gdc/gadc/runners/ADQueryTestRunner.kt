48670d685c39637d3d993836ec9599a42f401681
beppune <manzo.giuseppe@gmail.com>
2024-07-21 12:04:21 +0200

    add OperatorService::updateLocalPassword
    add admin user in CreateDatabaseRunner

--- src/main/kotlin/it/posteitaliane/gdc/gadc/runners/ADQueryTestRunner.kt
+++ src/main/kotlin/it/posteitaliane/gdc/gadc/runners/ADQueryTestRunner.kt
@@ -1,42 +1,21 @@
 package it.posteitaliane.gdc.gadc.runners
 
 import it.posteitaliane.gdc.gadc.config.ADConfig
-import it.posteitaliane.gdc.gadc.model.Operator
 import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.SpringApplication
 import org.springframework.context.ApplicationContext
 import org.springframework.context.annotation.Profile
-import org.springframework.ldap.core.AttributesMapper
-import org.springframework.ldap.core.LdapTemplate
-import org.springframework.ldap.core.support.LdapContextSource
-import org.springframework.ldap.query.LdapQueryBuilder.query
 import org.springframework.stereotype.Component
-import javax.naming.directory.Attributes
+import java.util.*
+import javax.naming.Context
+import javax.naming.PartialResultException
+import javax.naming.directory.InitialDirContext
+import javax.naming.directory.SearchControls
 
-data class SamaAccount(
-    val firstName:String,
-    val lastName:String,
-    val username:String,
-    val emails:List<String>
-)
-
-class SamaMapper : AttributesMapper<SamaAccount> {
-    override fun mapFromAttributes(attrs: Attributes?): SamaAccount {
-
-        return SamaAccount(
-            firstName = attrs?.get("givenName") as String,
-            lastName = attrs?.get("sn") as String,
-            emails = attrs?.get("proxyAddresses") as List<String>,
-            username = attrs?.get("cn") as String
-        )
-
-    }
-
-}
 
 @Component
-@Profile("test")
-class ADQueryTestRunner(private val config:ADConfig, private val ctx:ApplicationContext) : CommandLineRunner {
+@Profile("testldap")
+class ADQueryTestRunner(private val config: ADConfig, private val ctx: ApplicationContext) : CommandLineRunner {
     override fun run(vararg args: String?) {
 
         //example connection string
@@ -45,22 +24,45 @@ class ADQueryTestRunner(private val config:ADConfig, private val ctx:Application
         // search filter: (&(objectClass=person)(sAMAccountName=MANZOGI9))
         // search dn    :   DC=rete,DC=poste
 
-        val ldapCtx = LdapContextSource()
-            .apply {
-                setUrl("ldaps://${config.serverName}:636")
-                setBase(config.baseDN)
-                userDn = "RETE\\MANZOGI9"
-                password = "2Krum1r1"
-                authenticationSource
+        try {
+
+            var env = Hashtable<String, String>()
+                .apply {
+                    put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory")
+                    //put(Context.PROVIDER_URL, "ldaps://pretedc01v.rete.poste:636/DC=rete,DC=poste")
+                    //put(Context.SECURITY_PROTOCOL, "ssl")
+                    put(Context.PROVIDER_URL, "ldap://pretedc01v.rete.poste/DC=rete,DC=poste")
+                    put(Context.SECURITY_PRINCIPAL, "RETE\\MANZOGI9")
+                    put(Context.SECURITY_CREDENTIALS, "2Krum1r1")
+                    put(Context.SECURITY_AUTHENTICATION, "simple")
+                }
+
+            println("-----ALPHA")
+            var dir = InitialDirContext(env)
+
+            var attrs = SearchControls()
+                .apply {
+                    returningAttributes = arrayOf("cn", "sn", "givenName", "mail", "proxyAddresses")
+                    searchScope = SearchControls.SUBTREE_SCOPE
+                }
+
+            println("----BETA")
+            var answer = dir.search(/*"ou=utenti filiale,ou=filiale 1to,OU=Polo Torino"*/"", "(&(objectClass=person)(sAMAccountName=MANZOGI9))", attrs)
+
+            while (answer.hasMore()) {
+                val n = answer.next()
+                println(n.attributes.toString())
             }
 
-        val ldap = LdapTemplate(ldapCtx)
+            println("----GAMAM")
 
-        var q = query()
-            .base(config.baseDN)
-            .filter("(&(objectClass=person)(sAMAccountName=${ldapCtx.userDn}))")
 
-        ldap.search(q, SamaMapper()).also(::println)
+        }catch (ex:PartialResultException) {
+            //do nothing
+        }catch (ex:Exception) {
+            ex.printStackTrace()
+        }
+
 
         SpringApplication.exit(ctx)
     }
