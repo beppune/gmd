72b88627e81303e536f2463cd5b32aca2b2710a8
beppune <manzo.giuseppe@gmail.com>
2024-08-09 11:06:54 +0200

    OrderDetailsForm

--- src/main/kotlin/it/posteitaliane/gdc/gadc/views/TestView.kt
+++ src/main/kotlin/it/posteitaliane/gdc/gadc/views/TestView.kt
@@ -1,13 +1,12 @@
 package it.posteitaliane.gdc.gadc.views
 
-import com.vaadin.flow.component.button.Button
 import com.vaadin.flow.component.html.Div
-import com.vaadin.flow.component.notification.Notification
 import com.vaadin.flow.router.Route
 import it.posteitaliane.gdc.gadc.services.DatacenterService
 import it.posteitaliane.gdc.gadc.services.OrderService
 import it.posteitaliane.gdc.gadc.services.StorageService
-import it.posteitaliane.gdc.gadc.views.forms.ShippingForm
+import it.posteitaliane.gdc.gadc.services.SupplierService
+import it.posteitaliane.gdc.gadc.views.forms.OrderDetailsForm
 import jakarta.annotation.security.RolesAllowed
 
 
@@ -16,25 +15,14 @@ import jakarta.annotation.security.RolesAllowed
 class TestView(
     private val ss: StorageService,
     private val os:OrderService,
-    private val dcs:DatacenterService
+    private val dcs:DatacenterService,
+    private val sups:SupplierService
 ) : Div() {
 
 
     init {
-        val form = ShippingForm().apply { setWidth("40%") }
-        val button = Button("RESET") {
-            form.reset()
-        }
-        val button2 = Button("OK") {
-            val e = form.validate()
-            if(e.hasErrors()) {
-                return@Button
-            }
-
-            val c = form.compile()
-            Notification.show(c.toString())
-        }
-        add(form,button,button2)
+        val form = OrderDetailsForm(dcs,sups).apply { setWidth("40%") }
+        add(form)
     }
 
 
---
+++ src/main/kotlin/it/posteitaliane/gdc/gadc/views/forms/OrderDetailsForm.kt
@@ -0,0 +1,100 @@
+package it.posteitaliane.gdc.gadc.views.forms
+
+import com.vaadin.flow.component.combobox.ComboBox
+import com.vaadin.flow.component.formlayout.FormLayout
+import com.vaadin.flow.component.select.Select
+import com.vaadin.flow.data.binder.Binder
+import it.posteitaliane.gdc.gadc.model.Datacenter
+import it.posteitaliane.gdc.gadc.model.Order
+import it.posteitaliane.gdc.gadc.model.Supplier
+import it.posteitaliane.gdc.gadc.services.DatacenterService
+import it.posteitaliane.gdc.gadc.services.SupplierService
+
+data class OrderDetailsPresentation(
+    var type: Order.Type?=null,
+    var subject: Order.Subject?=null,
+    var datacenter: Datacenter?=null,
+    var supplier: Supplier?=null,
+
+)
+
+class OrderDetailsForm(
+    private val dcs: DatacenterService,
+    private val sups: SupplierService,
+): FormLayout() {
+
+    private val typeField = Select<Order.Type>()
+    private val subjectField = Select<Order.Subject>()
+    private val dcField = Select<Datacenter>()
+    private val supplierField = ComboBox<Supplier>()
+
+    private var bean = OrderPresentation()
+    val binder = Binder(OrderPresentation::class.java)
+
+
+    init {
+
+        binder.bean = bean
+
+        gui()
+
+        bind()
+    }
+
+    fun compile(): OrderPresentation? {
+        if(validate().isOk) {
+            return binder.bean
+        }
+
+        return null
+    }
+
+    fun validate() = binder.validate()
+
+    private fun bind() {
+        binder.forField(typeField)
+            .asRequired()
+            .bind({it.type},{odp,value->odp.type=value})
+
+        binder.forField(subjectField)
+            .asRequired()
+            .bind({it.subject},{odp,value->odp.subject=value})
+
+        binder.forField(dcField)
+            .asRequired()
+            .bind({it.datacenter},{odp,value->odp.datacenter=value})
+
+        binder.forField(supplierField)
+            .asRequired()
+            .bind({it.supplier},{odp,value->odp.supplier=value})
+    }
+
+    private fun gui() {
+        typeField.setItemLabelGenerator {
+            when(it) {
+                Order.Type.INBOUND -> "CARICO"
+                Order.Type.OUTBOUND -> "SCARICO"
+            }
+        }
+        typeField.setItems(Order.Type.values().toList())
+
+        subjectField.setItemLabelGenerator {
+            when(it) {
+                Order.Subject.INTERNAL -> "INTERNO"
+                Order.Subject.SUPPLIER -> "FORNITORE"
+                Order.Subject.SUPPLIER_DC -> "MOVING"
+            }
+        }
+        subjectField.setItems(Order.Subject.values().toList())
+
+        dcField.setItemLabelGenerator { "${it.short} - ${it.fullName}" }
+        dcField.setItems(dcs.findAll(true))
+
+        supplierField.setItems(sups.findAll())
+        supplierField.setItemLabelGenerator { it.name.uppercase() }
+
+        add(typeField, subjectField)
+        add(dcField, supplierField)
+    }
+
+}
