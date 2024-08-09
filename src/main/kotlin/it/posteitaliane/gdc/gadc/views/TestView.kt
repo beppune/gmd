b9d25a0e2222e8facceaa1b9b4dff3b533fb8cd2
beppune <manzo.giuseppe@gmail.com>
2024-08-09 15:30:52 +0200

    OrderDetailsForm

--- src/main/kotlin/it/posteitaliane/gdc/gadc/views/TestView.kt
+++ src/main/kotlin/it/posteitaliane/gdc/gadc/views/TestView.kt
@@ -1,11 +1,11 @@
 package it.posteitaliane.gdc.gadc.views
 
+import com.vaadin.flow.component.button.Button
 import com.vaadin.flow.component.html.Div
+import com.vaadin.flow.component.notification.Notification
 import com.vaadin.flow.router.Route
-import it.posteitaliane.gdc.gadc.services.DatacenterService
-import it.posteitaliane.gdc.gadc.services.OrderService
-import it.posteitaliane.gdc.gadc.services.StorageService
-import it.posteitaliane.gdc.gadc.services.SupplierService
+import it.posteitaliane.gdc.gadc.config.GMDConfig
+import it.posteitaliane.gdc.gadc.services.*
 import it.posteitaliane.gdc.gadc.views.forms.OrderDetailsForm
 import jakarta.annotation.security.RolesAllowed
 
@@ -16,13 +16,26 @@ class TestView(
     private val ss: StorageService,
     private val os:OrderService,
     private val dcs:DatacenterService,
-    private val sups:SupplierService
+    private val sups:SupplierService,
+    private val sec:SecurityService,
+    private val conf: GMDConfig
 ) : Div() {
 
 
     init {
-        val form = OrderDetailsForm(dcs,sups).apply { setWidth("40%") }
+        val form = OrderDetailsForm(
+            dcs = sec.op().permissions,
+            sups = sups.findAll(true),
+            firm = sups.findAll().filter { it.piva == conf.firmPiva }.first()
+        ).apply { setWidth("40%") }
+        form.addTypeChangeListener {
+            Notification.show(it.type.name)
+        }
         add(form)
+
+        val ok = Button("OK") { form.validate() }
+        val reset = Button("RESET") { form.reset() }
+        add(reset, ok)
     }
 
 
--- src/main/kotlin/it/posteitaliane/gdc/gadc/views/forms/OrderDetailsForm.kt
+++ src/main/kotlin/it/posteitaliane/gdc/gadc/views/forms/OrderDetailsForm.kt
@@ -1,5 +1,7 @@
 package it.posteitaliane.gdc.gadc.views.forms
 
+import com.vaadin.flow.component.ComponentEvent
+import com.vaadin.flow.component.ComponentEventListener
 import com.vaadin.flow.component.combobox.ComboBox
 import com.vaadin.flow.component.formlayout.FormLayout
 import com.vaadin.flow.component.select.Select
@@ -7,10 +9,8 @@ import com.vaadin.flow.data.binder.Binder
 import it.posteitaliane.gdc.gadc.model.Datacenter
 import it.posteitaliane.gdc.gadc.model.Order
 import it.posteitaliane.gdc.gadc.model.Supplier
-import it.posteitaliane.gdc.gadc.services.DatacenterService
-import it.posteitaliane.gdc.gadc.services.SupplierService
 
-data class OrderDetailsPresentation(
+data class OrderDetails(
     var type: Order.Type?=null,
     var subject: Order.Subject?=null,
     var datacenter: Datacenter?=null,
@@ -18,9 +18,19 @@ data class OrderDetailsPresentation(
 
 )
 
+class TypeChangeEvent(component: OrderDetailsForm, val type: Order.Type)
+    : ComponentEvent<OrderDetailsForm>(component, false)
+
+class SubjectChangeEvent(component: OrderDetailsForm, val subject: Order.Subject)
+    : ComponentEvent<OrderDetailsForm>(component, false)
+
+class DcChangeEvent(component: OrderDetailsForm, val dc: Datacenter)
+    : ComponentEvent<OrderDetailsForm>(component, false)
+
 class OrderDetailsForm(
-    private val dcs: DatacenterService,
-    private val sups: SupplierService,
+    private val dcs: List<Datacenter>,
+    private val sups: List<Supplier>,
+    private val firm: Supplier
 ): FormLayout() {
 
     private val typeField = Select<Order.Type>()
@@ -28,8 +38,8 @@ class OrderDetailsForm(
     private val dcField = Select<Datacenter>()
     private val supplierField = ComboBox<Supplier>()
 
-    private var bean = OrderPresentation()
-    val binder = Binder(OrderPresentation::class.java)
+    private var bean = OrderDetails()
+    val binder = Binder(OrderDetails::class.java)
 
 
     init {
@@ -41,7 +51,16 @@ class OrderDetailsForm(
         bind()
     }
 
-    fun compile(): OrderPresentation? {
+    fun addTypeChangeListener(listener: ComponentEventListener<TypeChangeEvent>) =
+        addListener(TypeChangeEvent::class.java, listener)
+
+    fun addSubjectChangeListener(listener: ComponentEventListener<SubjectChangeEvent>) =
+        addListener(SubjectChangeEvent::class.java, listener)
+
+    fun addDcChangeListener(listener: ComponentEventListener<DcChangeEvent>) =
+        addListener(DcChangeEvent::class.java, listener)
+
+    fun compile(): OrderDetails? {
         if(validate().isOk) {
             return binder.bean
         }
@@ -70,31 +89,67 @@ class OrderDetailsForm(
     }
 
     private fun gui() {
-        typeField.setItemLabelGenerator {
-            when(it) {
-                Order.Type.INBOUND -> "CARICO"
-                Order.Type.OUTBOUND -> "SCARICO"
+        typeField.apply {
+            placeholder = "OPERAZIONE"
+            setItemLabelGenerator {
+                when (it) {
+                    Order.Type.INBOUND -> "CARICO"
+                    Order.Type.OUTBOUND -> "SCARICO"
+                }
+            }
+
+            setItems(Order.Type.entries)
+
+            addValueChangeListener {
+                if(it.value != null ) {
+                    fireEvent(TypeChangeEvent(this@OrderDetailsForm, it.value))
+                }
             }
         }
-        typeField.setItems(Order.Type.values().toList())
 
-        subjectField.setItemLabelGenerator {
-            when(it) {
-                Order.Subject.INTERNAL -> "INTERNO"
-                Order.Subject.SUPPLIER -> "FORNITORE"
-                Order.Subject.SUPPLIER_DC -> "MOVING"
+        subjectField.apply {
+            placeholder = "TIPO"
+            setItemLabelGenerator {
+                when(it) {
+                    Order.Subject.INTERNAL -> "INTERNO"
+                    Order.Subject.SUPPLIER -> "FORNITORE"
+                    Order.Subject.SUPPLIER_DC -> "MOVING"
+                }
+            }
+            setItems(Order.Subject.INTERNAL, Order.Subject.SUPPLIER)
+
+            addValueChangeListener {
+                if(it.value!=Order.Subject.SUPPLIER) {
+                    supplierField.value = firm
+                    supplierField.isEnabled = false
+                } else {
+                    supplierField.clear()
+                    supplierField.isEnabled = true
+                }
             }
         }
-        subjectField.setItems(Order.Subject.values().toList())
 
-        dcField.setItemLabelGenerator { "${it.short} - ${it.fullName}" }
-        dcField.setItems(dcs.findAll(true))
+        dcField.apply {
+            placeholder = "DATACENTER"
+            setItemLabelGenerator { "${it.short} - ${it.fullName}" }
+            setItems(dcs)
+        }
 
-        supplierField.setItems(sups.findAll())
-        supplierField.setItemLabelGenerator { it.name.uppercase() }
+        supplierField.apply {
+            placeholder = "FORNITORE"
+            supplierField.setItems(sups)
+            supplierField.setItemLabelGenerator { it.name.uppercase() }
+        }
 
         add(typeField, subjectField)
         add(dcField, supplierField)
+    }
+
+    fun reset() {
+        bean = OrderDetails()
+        binder.bean = bean
+        supplierField.clear()
+        supplierField.isEnabled = true
     }
 
 }
