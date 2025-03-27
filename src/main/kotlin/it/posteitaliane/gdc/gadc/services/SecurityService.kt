a32062c057f6e6d9c4d3df728f5f566b4c7ae548
beppune <manzo.giuseppe@gmail.com>
2025-03-27 16:38:48 +0100

    refactor of getauthenticateduser

--- src/main/kotlin/it/posteitaliane/gdc/gadc/services/SecurityService.kt
+++ src/main/kotlin/it/posteitaliane/gdc/gadc/services/SecurityService.kt
@@ -4,31 +4,35 @@ import com.vaadin.flow.component.UI
 import com.vaadin.flow.server.VaadinServletRequest
 import com.vaadin.flow.spring.annotation.SpringComponent
 import it.posteitaliane.gdc.gadc.model.Operator
+import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.core.userdetails.User
 import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.crypto.factory.PasswordEncoderFactories
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler
 
 @SpringComponent
 class SecurityService(
+    private val db:JdbcTemplate,
     private val ops:OperatorService
 ) {
 
     private val LOGOUT_URL = "/"
 
-    fun getAuthenticatedUser() : UserDetails? {
-        val principal = SecurityContextHolder
-            .getContext()
-            .authentication.principal
+    fun getAuthenticatedUser() : UserDetails {
+        val auth = SecurityContextHolder.getContext().authentication
+        val username = auth.principal as String
+        val roles = db.queryForObject("SELECT role FROM operators WHERE uid = ?", String::class.java, username)
 
-        if( principal is UserDetails ) {
-            return principal
-        }
-
-        return null
+        return User.withUsername(username)
+            .passwordEncoder(PasswordEncoderFactories.createDelegatingPasswordEncoder()::encode)
+            .password("")
+            .roles(roles)
+            .build()
     }
 
     fun op() : Operator {
-        return ops.find(filter = getAuthenticatedUser()!!.username).first()
+        return ops.find(filter = getAuthenticatedUser().username).first()
     }
 
     fun logout() {
